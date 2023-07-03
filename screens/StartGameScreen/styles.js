7eac8c43d55790de133e7e7ffd98b3558c1c4c23
TretyakWeb <tretyakbro@gmail.com>
2023-07-04 00:02:22 +0300

    feat: add styles for StartGameScreen component
    Added a new file `screens/StartGameScreen/styles.js` to define the styles for the `StartGameScreen` component.

---
+++ screens/StartGameScreen/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  sukaContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowRadius: 6,
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 10, height: 10 },
+  },
+});
