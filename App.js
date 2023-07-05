2c7e181048e5c66301a0871a5fcd1cb2c88c838e
TretyakWeb <tretyakbro@gmail.com>
2023-07-06 02:46:47 +0300

    refactor: update App component
    Updated the App component in `App.js` to include a linear gradient background and an image background with the appropriate styles.

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
-import { StyleSheet, View } from "react-native";
+// Core
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+// Screen
 import { StartGameScreen } from "./screens";
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
-      <StartGameScreen />
-    </View>
+    <LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
+      <ImageBackground
+        style={styles.rootScreen}
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
