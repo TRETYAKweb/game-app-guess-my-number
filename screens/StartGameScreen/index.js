8961191ac7f89fdbb52744e65d8920b53e161141
TretyakWeb <tretyakbro@gmail.com>
2023-07-06 02:42:58 +0300

    feat: update StartGameScreen component
    Updated the StartGameScreen component to include number input handling, reset functionality, and validation for the entered number.

--- screens/StartGameScreen/index.js
+++ screens/StartGameScreen/index.js
@@ -1,9 +1,43 @@
-import { Text, TextInput, View } from "react-native";
+// Core
+import { useState } from "react";
+import { Alert, Text, TextInput, View } from "react-native";
 import { MainButton } from "../../components";
 
+// Styles
 import { styles } from "./styles";
 
 export const StartGameScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  const numberInputHandler = (inputText) => {
+    setEnteredNumber(inputText);
+  };
+
+  const resetHandler = () => {
+    setEnteredNumber("");
+  };
+
+  const confirmHandler = () => {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99.",
+        [
+          {
+            text: "Okey",
+            style: "destructive",
+            onPress: resetHandler,
+          },
+        ]
+      );
+      return;
+    }
+
+    console.log("Valid Number!");
+  };
+
   return (
     <View style={styles.sukaContainer}>
       <Text style={styles.startGameTitle}>Enter a Number</Text>
@@ -13,13 +47,15 @@ export const StartGameScreen = () => {
         keyboardType="number-pad"
         autoCapitalize="none"
         autoCorrect={false}
+        value={enteredNumber}
+        onChangeText={numberInputHandler}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <MainButton>Reset</MainButton>
+          <MainButton onPress={resetHandler}>Reset</MainButton>
         </View>
         <View style={styles.buttonContainer}>
-          <MainButton>Confirm</MainButton>
+          <MainButton onPress={confirmHandler}>Confirm</MainButton>
         </View>
       </View>
     </View>
