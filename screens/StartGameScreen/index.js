1703c9897d988c3bcee5fde8a79e5c7bf94ed75e
TretyakWeb <tretyakbro@gmail.com>
2023-07-05 04:21:39 +0300

    feat: update StartGameScreen component with number input and buttons
    Added a text component with a title for entering a number in the StartGameScreen component. Also, included a text input field for the number, along with reset and confirm buttons.

--- screens/StartGameScreen/index.js
+++ screens/StartGameScreen/index.js
@@ -1,4 +1,4 @@
-import { TextInput, View, Button } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { MainButton } from "../../components";
 
 import { styles } from "./styles";
@@ -6,9 +6,22 @@ import { styles } from "./styles";
 export const StartGameScreen = () => {
   return (
     <View style={styles.sukaContainer}>
-      <TextInput placeholder="test" />
-      <MainButton>Reset</MainButton>
-      <MainButton>Confirm</MainButton>
+      <Text style={styles.startGameTitle}>Enter a Number</Text>
+      <TextInput
+        style={styles.textInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <MainButton>Reset</MainButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <MainButton>Confirm</MainButton>
+        </View>
+      </View>
     </View>
   );
 };
