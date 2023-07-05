039540e53ee86f349a4317cf99bbd20be1683929
TretyakWeb <tretyakbro@gmail.com>
2023-07-05 04:22:36 +0300

    feat: update MainButton component
    Updated the MainButton component to include a press handler and styling for the button container, including pressed state styles.

--- components/MainButton/index.js
+++ components/MainButton/index.js
@@ -1,5 +1,24 @@
-import { Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
+import { styles } from "./styles";
 
 export const MainButton = ({ children }) => {
-  return <Text>{children}</Text>;
+  const pressHandler = () => {
+    return console.log("test");
+  };
+
+  return (
+    <View style={styles.btnOtherContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.btnInnerContainer, styles.pressed]
+            : styles.btnInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.btnText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 };
