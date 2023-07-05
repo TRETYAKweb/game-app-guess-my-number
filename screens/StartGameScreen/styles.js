308cf4075abe8a26f0a82baf7a6dd58637afad81
TretyakWeb <tretyakbro@gmail.com>
2023-07-05 04:20:55 +0300

    style: update styles for StartGameScreen component
    Updated the styles in `StartGameScreen/styles.js` to change the background color, add elevation, align items, and modify the styles for the start game title, text input, and buttons container.

--- screens/StartGameScreen/styles.js
+++ screens/StartGameScreen/styles.js
@@ -5,12 +5,36 @@ export const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     borderRadius: 8,
-    elevation: 4,
     shadowColor: "black",
     shadowRadius: 6,
     shadowOpacity: 0.2,
     shadowOffset: { width: 10, height: 10 },
+    elevation: 10,
+    alignItems: "center",
+  },
+
+  startGameTitle: {
+    color: "#ddb52f",
+    fontSize: 28,
+    fontWeight: "300",
+  },
+  textInput: {
+    width: 50,
+    height: 50,
+    textAlign: "center",
+    color: "#ddb52f",
+    borderBottomWidth: 2,
+    borderBottomColor: "#ddb52f",
+    fontSize: 32,
+    fontWeight: "700",
+    marginVertical: 8,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
