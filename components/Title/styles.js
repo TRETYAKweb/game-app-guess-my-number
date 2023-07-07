d0b752b9050171332e38b1e1dc84ef90b0e15045
TretyakWeb <tretyakbro@gmail.com>
2023-07-07 07:59:08 +0300

    feat: add Title component styles
    Added the `components/Title/styles.js` file to define the styles for the Title component.

---
+++ components/Title/styles.js
@@ -0,0 +1,17 @@
+// Core
+import { StyleSheet } from "react-native";
+
+// Constants
+import { colors } from "../../constants";
+
+export const styles = StyleSheet.create({
+  title: {
+    padding: 15,
+    fontSize: 24,
+    fontWeight: "bold",
+    color: colors.accent500,
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: colors.accent500,
+  },
+});
