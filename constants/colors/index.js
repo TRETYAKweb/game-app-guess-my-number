ac6cce569548dc9fecb3d1cdd66303ffc01705f6
TretyakWeb <tretyakbro@gmail.com>
2023-07-07 07:57:56 +0300

    feat: add colors constant
    Added the `colors` constant to the `constants/colors/index.js` file, providing predefined color values for the application.

---
+++ constants/colors/index.js
@@ -0,0 +1,10 @@
+export const colors = Object.freeze({
+  black: "#000000",
+  gray: "#808080",
+  white: "#ffffff",
+  main500: "#72063c",
+  main600: "#640233",
+  main700: "#4e0329",
+  main800: "#3b021f",
+  accent500: "#ddb52f",
+});
