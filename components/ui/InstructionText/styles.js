e1dd789a309bc4326ebe25019639949836709c9f
TretyakWeb <tretyakbro@gmail.com>
2023-07-09 15:53:05 +0300

    feat(UI): add styles for InstructionText component
    This commit adds the styles for the InstructionText component in the UI folder. The styles define the font family, color, and font size of the instructional text.

---
+++ components/ui/InstructionText/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+import { colors } from "../../../constants";
+
+export const styles = StyleSheet.create({
+  instructionText: {
+    fontFamily: "open-sans-regular",
+    color: colors.accent500,
+    fontSize: 28,
+  },
+});
