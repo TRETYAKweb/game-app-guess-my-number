d43bb5477d04902b2274bfa7b37a26053802fbbf
TretyakWeb <tretyakbro@gmail.com>
2023-07-11 17:15:19 +0300

    fix(components): add style prop

--- components/ui/MainButton/index.js
+++ components/ui/MainButton/index.js
@@ -1,19 +1,19 @@
 import { View, Text, Pressable } from "react-native";
 import { styles } from "./styles";
 
-export const MainButton = ({ children, onPress }) => {
+export const MainButton = ({ children, onPress, style }) => {
   return (
     <View style={styles.btnOtherContainer}>
       <Pressable
         style={({ pressed }) =>
           pressed
-            ? [styles.btnInnerContainer, styles.pressed]
-            : styles.btnInnerContainer
+            ? [[styles.btnInnerContainer, style?.btnContainer], styles.pressed]
+            : [styles.btnInnerContainer, style?.btnContainer]
         }
         onPress={onPress}
         android_ripple={styles.androidRipple}
       >
-        <Text style={styles.btnText}>{children}</Text>
+        <Text style={[styles.btnText, style?.btnText]}>{children}</Text>
       </Pressable>
     </View>
   );
