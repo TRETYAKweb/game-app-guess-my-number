10c8ea4128d2a6b96eb229d5058e9f67476ea647
TretyakWeb <tretyakbro@gmail.com>
2023-07-11 15:53:10 +0300

    feat(GameOverScreen): add component
    This commit adds the GameOverScreen component. The GameOverScreen component displays a game over message, the number of rounds it took to guess the number, and a button to start a new game. The component uses the Title, MainButton, Text, and Image components from React Native for the UI elements. The userNumber and guessRound props are used to display the relevant information. The component is styled using the styles and styleBtn objects defined in the styles.js file. The addition of this component enhances the user experience by providing feedback and options after the game is over.

--- screens/GameOverScreen/index.js
+++ screens/GameOverScreen/index.js
@@ -1,9 +1,28 @@
-import { View, Text } from "react-native";
+// Core
+import { View, Text, Image } from "react-native";
 
-export const GameOverScreen = () => {
+// Constants
+import { images } from "../../constants";
+
+// Components
+import { MainButton, Title } from "../../components";
+import { styles, styleBtn } from "./styles";
+
+export const GameOverScreen = ({ userNumber, guessRound, startNewGame }) => {
   return (
-    <View>
-      <Text>GameOverScreen</Text>
+    <View style={styles.rootContainer}>
+      <Title>Game Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={images.success} />
+      </View>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>{guessRound}</Text>{" "}
+        rounds to guess the number{" "}
+        <Text style={styles.highlight}>{userNumber}</Text>
+      </Text>
+      <MainButton style={styleBtn} onPress={startNewGame}>
+        Start New Game
+      </MainButton>
     </View>
   );
 };
