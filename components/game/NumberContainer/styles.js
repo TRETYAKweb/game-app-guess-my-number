792b2be0ce86c2e308eb405ca4ce66d5f8ed29b2
TretyakWeb <tretyakbro@gmail.com>
2023-07-12 21:37:35 +0300

    fix(styles): update NumberContainer styles
    This commit updates the styles in the NumberContainer component. The changes include adjusting the padding and margin based on the device width. The spacing styles now dynamically adapt to different screen sizes. Additionally, the font size of the number text has been adjusted for smaller devices to ensure better visibility. The styles are more responsive and provide a consistent user experience across various devices.

--- components/game/NumberContainer/styles.js
+++ components/game/NumberContainer/styles.js
@@ -1,10 +1,15 @@
+import { Dimensions } from "react-native";
 import { StyleSheet } from "react-native";
 import { colors } from "../../../constants";
 
+const deviceWidth = Dimensions.get("window").width;
+
+const spacingStyles = deviceWidth < 380 ? 12 : 24;
+
 export const styles = StyleSheet.create({
   container: {
-    padding: 25,
-    margin: 25,
+    padding: spacingStyles,
+    margin: spacingStyles,
     borderRadius: 8,
     borderWidth: 4,
     borderColor: colors.accent500,
@@ -14,6 +19,6 @@ export const styles = StyleSheet.create({
   numberText: {
     fontFamily: "open-sans-bold",
     color: colors.accent500,
-    fontSize: 36,
+    fontSize: deviceWidth < 380 ? 28 : 36,
   },
 });
