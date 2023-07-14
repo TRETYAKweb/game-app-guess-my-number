c833543c457e1288302eea0b218126c63aad31c7
TretyakWeb <tretyakbro@gmail.com>
2023-07-14 05:06:28 +0300

    feat(StartGameScreen): add KeyboardAvoidingView and ScrollView
    This commit adds the KeyboardAvoidingView and ScrollView components to the StartGameScreen. The KeyboardAvoidingView ensures that the screen content is adjusted when the keyboard is shown, preventing it from overlapping with the input field. The ScrollView allows the user to scroll the screen content if it exceeds the available height, providing a better user experience on smaller devices.

--- screens/StartGameScreen/index.js
+++ screens/StartGameScreen/index.js
@@ -1,6 +1,13 @@
 // Core
 import { useState } from "react";
-import { Alert, TextInput, View } from "react-native";
+import {
+  Alert,
+  TextInput,
+  View,
+  useWindowDimensions,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
 import { MainButton, Title, Card, InstructionText } from "../../components";
 
 // Styles
@@ -8,6 +15,7 @@ import { styles } from "./styles";
 
 export const StartGameScreen = ({ onPickNumber }) => {
   const [enteredNumber, setEnteredNumber] = useState("");
+  const { height } = useWindowDimensions();
 
   const numberInputHandler = (inputText) => {
     setEnteredNumber(inputText);
@@ -38,29 +46,35 @@ export const StartGameScreen = ({ onPickNumber }) => {
     onPickNumber(chosenNumber);
   };
 
+  const marginTop = height < 400 ? 30 : 100;
+
   return (
-    <View style={styles.rootContainer}>
-      <Title>Guess my number</Title>
-      <Card>
-        <InstructionText>Enter a Number</InstructionText>
-        <TextInput
-          style={styles.textInput}
-          maxLength={2}
-          keyboardType="number-pad"
-          autoCapitalize="none"
-          autoCorrect={false}
-          value={enteredNumber}
-          onChangeText={numberInputHandler}
-        />
-        <View style={styles.buttonsContainer}>
-          <View style={styles.buttonContainer}>
-            <MainButton onPress={resetHandler}>Reset</MainButton>
-          </View>
-          <View style={styles.buttonContainer}>
-            <MainButton onPress={confirmHandler}>Confirm</MainButton>
-          </View>
+    <ScrollView style={styles.screen}>
+      <KeyboardAvoidingView style={styles.screen} behavior="position">
+        <View style={[styles.rootContainer, { marginTop: marginTop }]}>
+          <Title>Guess my number</Title>
+          <Card>
+            <InstructionText>Enter a Number</InstructionText>
+            <TextInput
+              style={styles.textInput}
+              maxLength={2}
+              keyboardType="number-pad"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={enteredNumber}
+              onChangeText={numberInputHandler}
+            />
+            <View style={styles.buttonsContainer}>
+              <View style={styles.buttonContainer}>
+                <MainButton onPress={resetHandler}>Reset</MainButton>
+              </View>
+              <View style={styles.buttonContainer}>
+                <MainButton onPress={confirmHandler}>Confirm</MainButton>
+              </View>
+            </View>
+          </Card>
         </View>
-      </Card>
-    </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
