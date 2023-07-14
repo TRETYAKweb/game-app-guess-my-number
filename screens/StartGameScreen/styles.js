ce581599da0edf934f204cfbe72c94dbe354b951
TretyakWeb <tretyakbro@gmail.com>
2023-07-14 05:05:48 +0300

    style(StartGameScreen): update styles for screen and rootContainer
    This commit updates the styles for the StartGameScreen component. The screen style now has flex set to 1 to ensure the component expands to fill the available space. The rootContainer style now has marginTop set to 100 to create a top margin for the component.

--- screens/StartGameScreen/styles.js
+++ screens/StartGameScreen/styles.js
@@ -5,6 +5,9 @@ import { StyleSheet } from "react-native";
 import { colors } from "../../constants";
 
 export const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     marginTop: 100,
