fdfe759a788083a354c99c7cc54305625381b64b
TretyakWeb <tretyakbro@gmail.com>
2023-07-14 05:08:52 +0300

    refactor(GameOverScreen): improve layout and responsiveness
    This commit refactors the GameOverScreen component to improve its layout and responsiveness. The image size is adjusted based on the window dimensions to ensure proper display on different screen sizes. The content is rendered conditionally based on the width, with different layouts for widths above and below 500. The summaryText font size is adjusted for smaller widths. The Start New Game button is positioned below the image in the wider layout. Overall, the changes enhance the visual appearance and adaptability of the GameOverScreen component.

--- screens/GameOverScreen/index.js
+++ screens/GameOverScreen/index.js
@@ -1,5 +1,11 @@
 // Core
-import { View, Text, Image } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  useWindowDimensions,
+  ScrollView,
+} from "react-native";
 
 // Constants
 import { images } from "../../constants";
@@ -9,20 +15,76 @@ import { MainButton, Title } from "../../components";
 import { styles, styleBtn } from "./styles";
 
 export const GameOverScreen = ({ userNumber, guessRound, startNewGame }) => {
-  return (
-    <View style={styles.rootContainer}>
-      <Title>Game Over!</Title>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={images.success} />
+  const { width, height } = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 200;
+  }
+
+  if (height < 400) {
+    imageSize = 200;
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+    borderWidth: width < 380 ? 2 : 4,
+  };
+
+  let contentJSX = (
+    <>
+      <View style={[styles.rootContainer, { padding: width < 380 ? 30 : 24 }]}>
+        <Title>Game Over!</Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image style={styles.image} source={images.success} />
+        </View>
+        <Text style={[styles.summaryText, { fontSize: width < 380 ? 21 : 24 }]}>
+          Your phone needed <Text style={styles.highlight}>{guessRound}</Text>{" "}
+          rounds to guess the number{" "}
+          <Text style={styles.highlight}>{userNumber}</Text>
+        </Text>
+        <MainButton style={styleBtn} onPress={startNewGame}>
+          Start New Game
+        </MainButton>
       </View>
-      <Text style={styles.summaryText}>
-        Your phone needed <Text style={styles.highlight}>{guessRound}</Text>{" "}
-        rounds to guess the number{" "}
-        <Text style={styles.highlight}>{userNumber}</Text>
-      </Text>
-      <MainButton style={styleBtn} onPress={startNewGame}>
-        Start New Game
-      </MainButton>
-    </View>
+    </>
   );
+
+  if (width > 500) {
+    contentJSX = (
+      <>
+        <View
+          style={[styles.rootContainer, { padding: width < 380 ? 30 : 24 }]}
+        >
+          <Title>Game Over!</Title>
+          <View style={styles.imageTextContainer}>
+            <View style={[styles.imageContainer, imageStyle]}>
+              <Image style={styles.image} source={images.success} />
+            </View>
+            <View>
+              <Text
+                style={[
+                  styles.summaryText,
+                  { fontSize: 24, maxWidth: 400, textAlign: "left" },
+                ]}
+              >
+                Your phone needed{" "}
+                <Text style={styles.highlight}>{guessRound}</Text> rounds to
+                guess the number{" "}
+                <Text style={styles.highlight}>{userNumber}</Text>
+              </Text>
+              <MainButton style={styleBtn} onPress={startNewGame}>
+                Start New Game
+              </MainButton>
+            </View>
+          </View>
+        </View>
+      </>
+    );
+  }
+
+  return <ScrollView style={styles.screen}>{contentJSX}</ScrollView>;
 };
