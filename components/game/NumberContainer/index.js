e329914114c2bd55c11a8610e1c48fe267ae303f
TretyakWeb <tretyakbro@gmail.com>
2023-07-14 05:09:50 +0300

    feat(NumberContainer): add custom style prop
    This commit adds a custom style prop to the NumberContainer component. Now, you can pass a style object as a prop to customize the container's appearance. The style prop is applied to the container view, allowing for flexible styling options.

--- components/game/NumberContainer/index.js
+++ components/game/NumberContainer/index.js
@@ -1,9 +1,9 @@
 import { View, Text } from "react-native";
 import { styles } from "./styles";
 
-export const NumberContainer = ({ children }) => {
+export const NumberContainer = ({ children, style }) => {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, style]}>
       <Text style={styles.numberText}>{children}</Text>
     </View>
   );
